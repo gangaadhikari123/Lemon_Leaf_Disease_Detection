0a9d99feb9f784a5a54918881cf4480ab37c7000
kcbhawana07 <kcbhawana78@gmail.com>
2026-07-18 20:08:46 +0545

    backend code

--- bakend/predictor/serializers.py
+++
@@ -1,8 +0,0 @@
-# predictor/serializers.py
-from rest_framework import serializers
-from .models import PredictionRecord
-
-class PredictionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model  = PredictionRecord
-        fields = '__all__'
