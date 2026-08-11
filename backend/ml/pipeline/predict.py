26048b3d09082db68ce91e21f549fae9aaab52dd
Prasamsha Tripathi <prasamshatripathi@gmail.com>
2026-08-11 13:38:21 +0545

    report

--- backend/ml/pipeline/predict.py
+++ backend/ml/pipeline/predict.py
@@ -4,11 +4,10 @@ from tensorflow.keras.models import load_model
 
 MODEL_DIR = os.path.join(settings.BASE_DIR, "ml", "models")
 
-<<<<<<< HEAD
+
 leaf_model = load_model(os.path.join(MODEL_DIR, "leaf_detector_model.keras"))
-=======
 leaf_model = load_model(os.path.join(MODEL_DIR, "mobilenetv2_model1.keras"))
->>>>>>> 6640eb99dbb04553d8e1b6e19c189b5b1d4f964d
+
 disease_model = load_model(os.path.join(MODEL_DIR, "lemon_disease_model.keras"))
 
 class_names = [
@@ -21,11 +20,4 @@ class_names = [
     'Healthy Leaf',
     'Sooty Mould',
     'Spider Mites'
-<<<<<<< HEAD
 ]
-=======
-] 
-
-
-
->>>>>>> 6640eb99dbb04553d8e1b6e19c189b5b1d4f964d
